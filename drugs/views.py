c4830bd91e2a1db0b661f1948645051a05835676
AfzalAliSolangi <afzalsolangi.ali@gmail.com>
2024-04-16 00:26:01 +0500

    The key for manufacturer published

--- drugs/views.py
+++ drugs/views.py
@@ -285,7 +285,7 @@ def login_distributor(request):
         return render(request, "login_distributor.html")
 
 def login_check_distributor(request): #Implement Password authentication
-    print('login_check_manufacturer')
+    print('login_check_distributor')
     if request.method == 'POST':
         email_rcvd = request.POST.get('email')
         password_rcvd = request.POST.get('passw')
@@ -436,13 +436,17 @@ def products(request):
     print(selected_manufacturer)
     x = rpc_connection.subscribe('{}'.format(users_manufacturer_items_stream)) #subscribing
     response = rpc_connection.liststreamkeyitems('{}'.format(users_manufacturer_items_stream), '{}'.format(selected_manufacturer))#Based on the manufacturer KEY the data is being fetched
-    response = response[-1]# always fetches the latest record of medicines from the manufacturer stream
-    json_string = json.dumps(response, indent=4) #Converts OrderedDict to JSON String
-    response = {} #Cleaning the unused response
-    print(json_string)
-    json_load = json.loads(json_string)
-    products = json_load['data']['json']['products']
-    return render(request, 'products.html', {'products': products,'manufacturer' : selected_manufacturer})
+    print(len(response))
+    if len(response)>0:
+        response = response[-1]# always fetches the latest record of medicines from the manufacturer stream
+        json_string = json.dumps(response, indent=4) #Converts OrderedDict to JSON String
+        response = {} #Cleaning the unused response
+        print(json_string)
+        json_load = json.loads(json_string)
+        products = json_load['data']['json']['products']
+        return render(request, 'products.html', {'products': products,'manufacturer' : selected_manufacturer})
+    else:
+        return render(request, 'products.html', {'message': 'No products available'})
 
 @csrf_protect
 def checkout(request):
